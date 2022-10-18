2ae1adc2ed431eb638b83202ad5d4dffc354cad9
tim.cehovin <tim.cehovin@gmail.com>
2022-10-18 12:56:46 +0000

    Drugi commit

--- myapp/views.py
+++ myapp/views.py
@@ -7,7 +7,7 @@ def Register_view(request):
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
-        form = UserForm(request.POST)
+        form = UserCreationForm(request.POST)
         # check whether it's valid:
         if form.is_valid():
             # process the data in form.cleaned_data as required
@@ -17,6 +17,6 @@ def Register_view(request):
 
     # if a GET (or any other method) we'll create a blank form
     else:
-        form = UserForm()
+        form = UserCreationForm()
 
     return render(request, 'registration.html', {'form': form})
